4348fed313d03b8b1a3040fefdc98f282b12105f
Rub <melikyan740@gmail.com>
2022-09-27 16:17:22 -0700

    Edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -2,35 +2,41 @@ const React = require("react");
 const Default = require("./layouts/default")
 
 
-function show ({bread}, index) {
-    // Confirm we are getting our bread data in the terminal.
-     console.log(bread.name)
-      return (
-        <Default>
-        <h3>{bread.name}</h3>
-        <p>
-          and it
-          {
-            bread.hasGluten
+function show({ bread }, index) {
+  // Confirm we are getting our bread data in the terminal.
+  console.log(bread.name)
+  return (
+    <Default>
+      <h3>{bread.name}</h3>
+      <p>
+        and it
+        {
+          bread.hasGluten
             ? <span> does </span>
             : <span> does NOT </span>
-          }
-          have gluten.
-        </p>
-        <img src={bread.image} alt={bread.name} />
-        <li><a href="/breads">Go home</a></li>
+        }
+        have gluten.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+      <li><a href="/breads">Go home</a></li>
 
 
-        <form action={`/breads/${index}?_method=DELETE`} method="POST">
-  <input type='submit' value="DELETE"/>
-</form>
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type='submit' value="DELETE" />
+      </form>
+
+     
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
 
-      </Default>
-      
-      )
-  }
-  
+     
+
+
+    </Default>
+
+  )
+}
+
 
 
 module.exports = show
